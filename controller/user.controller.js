9f043d9e2dec2c21a4e1ec82b296c0e9cc3bc663
mohorgoswami <prajnagoswami22@gmail.com>
2024-07-06 09:43:00 +0530

    I have changed in user controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,66 +1,57 @@
 const { createUser, verifyEmail } = require("../db/context/user.context");
-const { handleSuccessResponse, handleErrorResponse, handleCustomErrorResponse } = require("../utils/handlers/errorResponseHandlers");
+const {
+  handleSuccessResponse,
+  handleErrorResponse,
+  handleCustomErrorResponse,
+} = require("../utils/handlers/errorResponseHandlers");
 
 const createUsers = async (req, res) => {
   try {
     const isEmail = await verifyEmail(req.body.email);
     if (isEmail) {
-      return handleCustomErrorResponse(res, "Given email is already available, please try another one.", 409);
+      return handleCustomErrorResponse(
+        res,
+        "Given email is already available, please try another one.",
+        409
+      );
     }
     const users = await createUser({ ...req.body });
     return handleSuccessResponse(res, "User created successfully", users, 201);
   } catch (error) {
     return handleErrorResponse(error, req, res, "Failed to create user");
-// const createUser = async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     const user = await User.create({ name, email, password });
-//     res.status(201).json(user);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).json({ error: error.message });
-//   }
-// };
+  }
+};
 
-
-const getUserById=async(req,res)=>{
-  const {id}=req.params;
-  try{
-    const user=await User.findByPk(id);
-    if(!user){
-      res.status(404).json({message:'User not found'});
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
     }
     res.status(200).json(user);
-    
-
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
-    res.status(500).json({message:"Error occurred"});
-
+    res.status(500).json({ message: "Error occurred" });
   }
-}
+};
 
-const updateUserById=async(req,res)=>{
-  const {id}=req.params;
-  const{name,role}=req.body;
-  try{
-    const user=await User.findByPk(id);
-    if(!user){
-      res.status(404).json({message:'User not found'});
+const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const { name, role } = req.body;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
     }
-  if(name) 
-    user.name=name;
-  user.role=role;
-  await user.save();
+    if (name) user.name = name;
+    user.role = role;
+    await user.save();
 
-    res.status(200).json({ message: 'User updated successfully', user });
-
-    }
-  catch(error){
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
-
-module.exports = { {getUserById,updateUserById}s };
+module.exports = { createUsers, getUserById, updateUserById };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { {getUserById,updateUserById}s } = require("../controller/user.controller");
+const {
+  getUserById,
+  updateUserById,
+  createUsers,
+} = require("../controller/user.controller");
 
-const { validateRequestBody } = require('../middleware/validateMiddleware');
-const { createUserSchema } = require("../validators/user.validators")
+const { validateRequestBody } = require("../middleware/validateMiddleware");
+const { createUserSchema } = require("../validators/user.validators");
 
-
- router.get('/user/:id', getUserById);
- router.put('/update/:id',updateUserById);
-
-
+router.get("/user/:id", getUserById);
+router.put("/update/:id", updateUserById);
+router.post("/create", validateRequestBody(createUserSchema), createUsers);
 
 module.exports = router;
